8aaba77290edce062d1f51e9a94bce87c2144131
Pawel Maslanka <pawmas@hotmail.com>
2018-09-05 00:39:29 +0200

    Added comments how to implement system-dependent procedures

--- include/stp/system.hpp
+++ include/stp/system.hpp
@@ -10,11 +10,26 @@
 
 namespace Stp {
 
+/**
+ * @brief The OutInterface class's procedures model the system-dependent actions.
+ */
 class OutInterface {
 public:
+    /**
+     * @brief FlushFdb removes all entries for the port from the filtering database
+     */
     virtual Result FlushFdb(const u16 portNo) __noexcept = 0;
+    /**
+     * @brief SetForwarding starts or stop forwarding frames through the port
+     */
     virtual Result SetForwarding(const u16 portNo, const bool enable) __noexcept = 0;
+    /**
+     * @brief SetLearning starts or stops learning from frames received on the port
+     */
     virtual Result SetLearning(const u16 portNo, const bool enable) __noexcept = 0;
+    /**
+     * @brief SendOutBpdu transmits frame through the port
+     */
     virtual Result SendOutBpdu(const u16 portNo, ByteStreamH data) __noexcept = 0;
 
 protected:
